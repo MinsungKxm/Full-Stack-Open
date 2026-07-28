05b35165d210b4f30cbb5827f199956415036372
MinsungKxm <minsungk101@gmail.com>
2026-07-28 17:02:24 -0400

    Added components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,75 @@
-const App = () => {
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
+const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
     <div>
-      <p>Hello world</p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   )
+}
+
+const Total = ({ total }) => {
+  return <p>Number of exercises {total}</p>
+}
+
+const Hello = (props) => {
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+
 }
 
 export default App
