cc591b9682f5a2fc76cd9ba03dfffc91cc11f19c
MinsungKxm <minsungk101@gmail.com>
2026-07-29 16:38:17 -0400

    Completed part 1 exercises

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,75 +1,128 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
+import { useState } from "react";
 
-const Part = ({ part, exercises }) => {
+const Button = ({ text, onClick }) => {
   return (
-    <p>
-      {part} {exercises}
-    </p>
-  )
-}
+    <button onClick={onClick}>
+      {text}
+    </button>
+  );
+};
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
-    </div>
-  )
-}
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
 
-const Total = ({ total }) => {
-  return <p>Number of exercises {total}</p>
-}
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
 
-const Hello = (props) => {
-
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
+  if (all === 0) {
+    return <p>No feedback given</p>;
   }
 
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
 
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={`${positive} %`} />
+      </tbody>
+    </table>
+  );
+};
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  const [votes, setVotes] = useState(
+    Array(anecdotes.length).fill(0)
+  );
+
+  const handleNextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const handleVote = () => {
+    const copy = [...votes];
+
+    copy[selected] += 1;
+
+    setVotes(copy);
+  };
+
+  const mostVotes = Math.max(...votes);
+  const mostVotedIndex = votes.indexOf(mostVotes);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Give feedback</h1>
+
+      <Button
+        text="good"
+        onClick={() => setGood(good + 1)}
+      />
+
+      <Button
+        text="neutral"
+        onClick={() => setNeutral(neutral + 1)}
+      />
+
+      <Button
+        text="bad"
+        onClick={() => setBad(bad + 1)}
+      />
+
+      <h1>Statistics</h1>
+
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
+
+      <h1>Anecdote</h1>
+
+      <p>{anecdotes[selected]}</p>
+
+      <p>has {votes[selected]} votes</p>
+
+      <Button
+        text="vote"
+        onClick={handleVote}
+      />
+
+      <Button
+        text="Next anecdote"
+        onClick={handleNextAnecdote}
+      />
     </div>
-  )
+  );
+};
 
-}
-
-export default App
+export default App;
